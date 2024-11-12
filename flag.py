a24d3125486e50b6334d311d3845a8fd11b97ec5
Rafita261 <154252381+Rafita261@users.noreply.github.com>
2024-11-12 06:53:57 +0100

    Update flag.py

--- flag.py
+++ flag.py
@@ -1,13 +1,5 @@
-import math
-def flag(n) :
-    k=""
-    for elem in n:
-        k+=chr(int(ord(elem)*math.log(ord(elem)))-2*ord(elem))
-    return k
-
-
-test = input("Saisir votre chance :\n ")
-if flag(test)=="Ì£§ÈřĖÈòÝYÏÅòċ±ù š" :
-    print("Arabaina !!!")
-else :
-    print("Manandrama hafa")
+### There is no flag
+'''
+Just search somewhere
+'''
+print("....")
